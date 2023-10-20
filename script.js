7979a193080212894e18ec453aa9f761466a8b6a
adrian78pkk <144406577+adrian78pkk@users.noreply.github.com>
2023-10-20 19:14:00 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+const modeLabel = document.getElementById('mode-label');
+
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        body.classList.add('dark-mode');
+        modeLabel.textContent = 'Mode Gelap';
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        body.classList.remove('dark-mode');
+        modeLabel.textContent = 'Mode Terang';
+        localStorage.setItem('dark-mode', 'disabled');
+    }
+});
+
+// Check local storage for user preference
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+    modeLabel.textContent = 'Mode Gelap';
+}
